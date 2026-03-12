670c73a814692fab5b3d0dac4941750d2a3be76a
Guilherme-GVDS <vini.guilherme1800@gmail.com>
2026-03-12 10:53:00 -0300

    feat: add logo and filter available spots and unparked vehicles

--- parking/admin.py
+++ parking/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from vehicles.models import Vehicle
 from .models import ParkingSpot, ParkingRecord
 
 
@@ -14,3 +16,19 @@ class ParkingRecordAdmin(admin.ModelAdmin):
     list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
     search_fields = ['vehicle__license_plate', 'parking_spot__spot_number']
     list_filter = ['entry_time', 'exit_time']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parking_spot':
+            if not request.resolver_match.kwargs.get('object_id'):
+                kwargs['queryset'] = ParkingSpot.objects.filter(
+                    is_occupied=False
+                )
+        elif db_field.name == 'vehicle':
+            if not request.resolver_match.kwargs.get('object_id'):
+                parked_vehicles = ParkingRecord.objects.filter(
+                    exit_time__isnull=True
+                ).values_list('vehicle_id', flat=True)
+                kwargs['queryset'] = Vehicle.objects.exclude(
+                    id__in=parked_vehicles
+                )
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
