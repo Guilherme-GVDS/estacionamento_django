a1fb61f84b26475b738f987862ff9b7826d7fa4c
Guilherme-GVDS <vini.guilherme1800@gmail.com>
2026-03-18 10:49:29 -0300

    feat: add Docker and docker-compose configuration

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,17 @@ from django.urls import include, path
 
 from drf_spectacular.views import (SpectacularAPIView,
                                    SpectacularRedocView,
-                                   SpectacularSwaggerView
-)
+                                   SpectacularSwaggerView,
+                                   )
 
 urlpatterns = [
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'
+         ),
+    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'
+         ),
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('customers.urls')),
     path('api/v1/', include('vehicles.urls')),
