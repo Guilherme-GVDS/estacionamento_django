b684b503ec11308e18fb89b9367b93773114d198
Guilherme-GVDS <vini.guilherme1800@gmail.com>
2026-03-23 10:31:56 -0300

    fix: configure csrf trusted origins for production

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +30,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
